c3301e15c0c6caee14ee9d253496f26f5065ef7f
zaxllpjm <143859963+zaxllpjm@users.noreply.github.com>
2023-09-01 20:59:42 -0700

    uploud file

---
+++ latihan1.py
@@ -0,0 +1,20 @@
+nama_saya = str(input("zendia ameelia = "))
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = str(input("18 = "))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input)("4.00 = "))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input)("Sudah menikah? = "))
+print("menikah?" ,menikah, " bertipe", type(menikah))
+
+matkul_diambil = ["alpro, "sig"]
+print("matkul" : ",matkul_diambil, "bertipe", type(matkul_diambil))
+      
+matkul_diambil2 = ("web, "pemvis")
+print("matkul" : ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+      
+matkul_diambil3 = {"Alpro, "Alpro" "pemWeb", "Data Science", "Data Science"}
+print(Matkul lain -lain: 'matkul_diambil3, "bertipe", type(matkul_diambil3))
